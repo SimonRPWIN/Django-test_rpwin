8d8790833f8fbad92e24b45e097226a690262d3b
Simon Ren <109325408+SimonRPWIN@users.noreply.github.com>
2023-02-10 23:27:53 +1300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from redis import Redis
+
+redis_cli = Redis(host='localhost',
+                  port='6379',
+                  db=0
+                  )
+
+redis_cli.set('iloveu','china')
+
+print(redis_cli.get('iloveu'))
